ebbad9700103f9f01c2bc5530c6547250144da83
230043 Michalina Wysocka <230043@edu.p.lodz.pl>
2023-12-15 15:12:18 +0100

    Added base components

---
+++ frontend/src/components/GameChooseHUB.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <GameTile v-for="game in availableGames" :key="game.id" :game="game" />
+  </div>
+</template>
+
+<script>
+import GameTile from './GameTile.vue'
+export default {
+  name: 'GameChooseHUB',
+  components: {
+    GameTile
+  },
+  data() {
+    return {
+      availableGames: [],
+    }
+  },
+  methods: {
+    displayGames() {
+
+    },
+    selectGame() {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
