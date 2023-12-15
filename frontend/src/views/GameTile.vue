68a05a382a08cd7c0aba3247966fe8d73d8a9d61
230043 Michalina Wysocka <230043@edu.p.lodz.pl>
2023-12-15 17:39:17 +0100

    Added GameTile methods

--- frontend/src/views/GameTile.vue
+++ frontend/src/views/GameTile.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="game-tile">
-    <img class="game-icon" src="" alt="Minigame Icon" @click="displayGameInfo"/>
+    <img class="game-icon" src="" alt="Minigame Icon" @click=""/>
+    <Minigame ref="minigame"/>
+    <Quiz ref="quiz"/>
+    <div v-if="scores.length">
+      <button @click="displayScoreboard">Display Scoreboard</button>
+      <h2>Scoreboard</h2>
+      <div v-for="score in scores" :key="score.playerID">
+        <span>PlayerID: {{ score.playerID }},</span>
+        <span>Score: {{ score.score }}</span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,26 +18,55 @@
 // kuźwa ciężko cokolwiek napisać nie mając Quizu i Minigame
 export default {
   name: 'GameTile',
+  data() {
+    return {
+      scores: [],
+    }
+  },
+  components: {
+    Minigame: {
+      template: '<div>Mock Minigame</div>',
+      methods: {
+        start() {
+          console.log('Minigame started');
+        },
+      }
+    },
+    Quiz: {
+      template: '<div>Mock Quiz</div>',
+      data() {
+        return {
+          isFinished: false
+        }
+      }
+    },
+  },
   methods: {
     startGame() {
+      if (this.$refs.quiz.isFinished) {
+        this.$refs.minigame.start();
+      } else {
+        this.displayMessage('Quiz is not finished yet!');
+      }
     },
 
+    // na razie tak
     getScores() {
-    },
-
-    displayGameInfo() {
+      return [
+        {playerID: 'player1', score: 10},
+        {playerID: 'player2', score: 20},
+        {playerID: 'player3', score: 30},
+      ];
     },
 
     displayScoreboard() {
-
+      this.scores = this.getScores();
     },
 
-    displayMessage() {
-
+    // na razie tak
+    displayMessage(message) {
+      console.log(message);
     },
-
-    getQuizStatus() {
-    }
   }
 }
 </script>
